a106c3f11015d38e5b5bb70bc5abe6c9b7ad0c47
ThomasZadikian <110538223+ThomasZadikian@users.noreply.github.com>
2025-08-22 10:21:36 +0200

    init alembic

--- src/main.py
+++ src/main.py
@@ -1,25 +1,10 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-
 from src.infrastructure.api import livre_router
-from src.infrastructure.persistance.database import engine
-from src.infrastructure.persistance.models import Base
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
 
 app = FastAPI(
     title="Écrin Littéraire",
     description="API pour la gestion de l'Écrin Littéraire",
-    version="1.0.0",
-    lifespan=lifespan
+    version="1.0.0"
 )
 
 app.include_router(livre_router.router)
